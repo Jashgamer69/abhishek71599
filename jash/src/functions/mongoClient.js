f07e725da6040060f59107a124f696ada5048b57
Jashgamer69 <102601888+Jashgamer69@users.noreply.github.com>
2023-04-23 14:43:57 +0530

    Update mongoClient.js

--- jash/src/functions/mongoClient.js
+++ jash/src/functions/mongoClient.js
@@ -3,4 +3,4 @@ const env = require('../env')
 const client = new MongoClient(env.mongoLink);
 
 exports.connect = () => client.connect();
-exports.db = client.db("ABot");
+exports.db = client.db("ABot"+env.bot_token.split(":")[0]);
