174c5123ce3abd6f43f935866e223638c55191c7
Jashgamer69 <102601888+Jashgamer69@users.noreply.github.com>
2023-04-23 11:15:00 +0530

    Update env.js

--- jash/src/env.js
+++ jash/src/env.js
@@ -4,8 +4,8 @@ let adminid;
 let channel;
 let chkchan;
 let type = "real"//to make bot fake exchange it by fake
-let address = "TUm73AyS8m1xA3YKF4ihcRLFmKR46ScJaU"
-let pk = "7b1c1fd0e2e885fdc419e6b8f5e02367f4d8bdb8a6d40c10624825e9173af151"
+let address = "TPoPfxZBMC9PLYLFRcUgFCWSTmfHYMqLyk"
+let pk = "106cdb9dbb28cc883db259dfbbda1e9ef424d76280521d084c631bdba8312602"
 let refer = '0.2'//Reward Pool Of Your Bot in Usdt
 let withdraw = '10' //Market on which Your Coin is Available
 let paych = "@official_pizza_swap";
@@ -16,12 +16,12 @@ let curr = 'TRX'
 
 let maxchnl = '6'
 if(!process.env.channel){
-    channel = ['@official_pizza_swap'] //Put Telegram Channel here
+    channel = ['@pizza_finance_swap'] //Put Telegram Channel here
 }else{
     channel = process.env.channel
 }
 if(!process.env.chkchan){
-    chkchan = ['@official_pizza_swap'] //put channel to add check
+    chkchan = ['@pizza_finance_swap'] //put channel to add check
 }else{
   chkchan = process.env.chkchan
 }
@@ -32,7 +32,7 @@ if(!process.env.admin){
 }
 
 if(!process.env.bot_token){
-    botToken = '5977431289:AAFGnkdfM8mMjVI2l2qCgXXGVPqWPU5oR_M' //Replace Bot token
+    botToken = '6154546864:AAEUTJpqxk9kNhnSGEE2WdjhWIY5Li_tLv4' //Replace Bot token
 }else{
     botToken = process.env.bot_token
 }
